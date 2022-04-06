c5f5ecf61e601c040a87eca1195a12c1c1c220c8
KANIKA <kanikaadik07@gmail.com>
2022-04-06 01:14:34 -0400

    updated run.py

--- run.py
+++ run.py
@@ -1,55 +1,79 @@
-import sys
 import shift
-import time
-
-print("lising Shitf details:", dir(shift))
-
-tickers =['AAPL', 'AXP', 'BA', 'CAT', 'CSCO', 'CVX', 'DIA', 'DIS', 'GS', 'HD', 'IBM', 'INTC', 'JNJ', 'JPM', 'KO', 'MCD', 'MMM', 'MRK', 'MSFT', 'NKE', 'PFE', 'PG', 'SPY', 'TRV', 'UNH', 'V', 'VZ', 'WBA', 'WMT', 'XOM']
+from time import sleep
+import datetime as dt
 
 
-def main(argv):
-    # create trader object
-    trader = shift.Trader("swift_trade")
-    # connect and subscribe to all available order books
-    try:
-        trader.connect("initiator.cfg", "crT4Y3w9")
-        
-        trader.sub_all_order_book()
-        time.sleep(5)
-        print("Last trader time: ", trader.get_last_trade_time())
-        trader.get_subscribed_order_book_list()
-        print("tick, trader.get_last_size(tick), trader.get_last_price(tick), trader.get_close_price(tick), trader.get_close_price(tick)")
-        for tick in tickers:
-            print("%s :%s , %s, %s, %s "%(tick, trader.get_last_size(tick), trader.get_last_price(tick), trader.get_close_price(tick), trader.get_close_price(tick))) 
-        print(trader.get_last_price('AAPL'))
-        print(trader.get_close_price())
-        print(trader.get_best_price())
-        print(trader.get)
-        print(trader.get_portfolio_summary())
-    except Exception as e:
-        print("Excption occured!: ", e)
-    except shift.IncorrectPasswordError as e:
-        print(e)
-    except shift.ConnectionTimeoutError as e:
-        print(e)
+def connect(username, password):
+     print ("Connecting website -- 155.246.104.90 , %s %s"%(username, password))
+     trader = shift.Trader(username)
+     try:
+          trader.connect("initiator.cfg",password)
+          sleep(10)
+          trader.sub_all_order_book()
+     except Exception as e:
+          print("Exception occurred!!! - %s" % e)
+     return trader
 
-    # demo_01(trader)
-    # demo_02(trader)
-    # demo_03(trader)
-    # demo_04(trader)
-    # demo_05(trader)
-    # demo_06(trader)
-    # demo_07(trader)
-    # demo_08(trader)
-    # demo_09(trader)
-    # demo_10(trader)
+def market_open(trader):
+    d = trader.get_last_trade_time()
+    start_time = dt.time(9,30,0)
+    print(" trade time :", start_time < trader.get_last_trade_time().time() )
+    while start_time > trader.get_last_trade_time().time():
+         print("Market not yet open at ", trader.get_last_trade_time())
+         sleep(10)
+    return True
+         
 
-    # disconnect
-    trader.disconnect()
+def start_trading(trader):
+     if trader.is_connected():
+         trader.sub_all_order_book()
+         trader.get_subscribed_order_book_list()
+         limit_buy = shift.Order(shift.Order.Type.LIMIT_BUY, "AAPL", 1, 10.00)
+         trader.submit_order(limit_buy)
+         print("Buying Power\tTotal Shares\tTotal P&L\tTimestamp")
+         print("%12.2f\t%12d\t%9.2f\t%26s"
+         % (
+            trader.get_portfolio_summary().get_total_bp(),
+            trader.get_portfolio_summary().get_total_shares(),
+            trader.get_portfolio_summary().get_total_realized_pl(),
+            trader.get_portfolio_summary().get_timestamp(),
+         ))
 
-    return
+         print("Symbol\t\tShares\t\tPrice\t\t  P&L\tTimestamp")
+         for item in trader.get_portfolio_items().values():
+            print(
+            "%6s\t\t%6d\t%9.2f\t%9.2f\t%26s"
+             %(
+                item.get_symbol(),
+                item.get_shares(),
+                item.get_price(),
+                item.get_realized_pl(),
+                item.get_timestamp(),
+             )
+         )
+         print("Waiting list orders :", trader.get_waiting_list())
+         return 
+         for order in trader.get_waiting_list():
+             trader.submit_cancellation(order)
 
-
-if __name__ == "__main__":
-    main(sys.argv)
-
+ 
+def conclude():
+     print("Exit all positions")
+      
+"""
+Starting with basic execution -
+1. Connect to the the stock exchange
+2. Check Timings to start tracking tickers
+3. Get previous ticker close price 
+4. Get Previous tickers Volume
+5. Build a strategy to execute for one ticker one thread 
+"""
+if __name__ == '__main__' :
+     trader = connect('swift_trade', 'crT4Y3w9')
+     if market_open(trader):
+         #start_timer() #shoudl notify once end time is approaching
+         start_trading(trader)
+     
+         conclude()
+     trader.disconnect()
+     
